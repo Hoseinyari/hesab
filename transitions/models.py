7ffc0e0ab81424cc6c8fa047289dcad507604b20
Hoseinyari <jwadyary88@gmail.com>
2025-01-05 22:37:05 +0330

    16 dey

--- transitions/models.py
+++ transitions/models.py
@@ -4,14 +4,16 @@ from django.db import models
 
 class Category(models.Model):
     name= models.CharField(max_length=200)
+    def ___str__(self):
+        return self.title
     
 class Transitions(models.Model):
     text = models.CharField(max_length=300)
     amounth = models.PositiveBigIntegerField()
     date = models.DateTimeField(auto_now=True , editable=False)
     category = models.ForeignKey(Category , on_delete=models.PROTECT)
-    def __unicode__(self):
-        return '{}-{}'.format(self.date , self.amounth)
+    def ___str__(self):
+        return self.title
     
 
 # class Sort(models.Model):
