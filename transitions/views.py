4f5fb130131891a345ee7493d00f61fa404a4f91
Hoseinyari <jwadyary88@gmail.com>
2025-01-14 17:26:38 +0330

    form changed

--- transitions/views.py
+++ transitions/views.py
@@ -1,31 +1,32 @@
-from datetime import date 
-from django.shortcuts import render, redirect 
-from django.http import HttpResponse 
+from django.shortcuts import render 
+from django.http import HttpResponse ,HttpResponseRedirect
 from transitions.models import Transition
+from django.urls import reverse
 
 # Create your views here. 
 
 def home_view(request): 
-    all_transitions = Transition.objects.all() 
-    return render(request,"transitions/all_transitions.html",{"all_transitions": all_transitions})
+    all_transitions = Transition.objects.all().order_by("-date")
+    return render(request,'transitions/all_transitions.html',{"all_transitions": all_transitions})
 
 def expose_view(request): 
     exposes = Transition.objects.filter(category='expose').values() 
     return render(request, "transitions/expose.html", {"exposes": list(exposes)}) 
 
 def income_view(request): 
-    incomes = Transition.objects.filter(category='income').values() 
+    incomes = Transition.objects.filter(category='income').values()
     return render(request, "transitions/income.html", {"incomes": list(incomes)}) 
 
 def add_transitions(request): 
     if request.method == 'POST': 
-        transition= Transition(
-            text=request.POST["text"], 
-            amount=request.POST["amount"], 
-            date=request.POST["date"], 
-            category=request.POST["category"], 
+        form = request.post
+        new_transition= Transition(
+            text=form['text'], 
+            amount=form['amounth'], 
+            date=form['date'], 
+            category=form['category'], 
         ) 
-        transition.save() 
-        return redirect('home_view') 
-
-    return render(request, 'transitions/add_transitions.html')
+        new_transition.save() 
+        return HttpResponseRedirect(reverse('home_view')) 
+    
+    return render(request,'transitions/add_transitions.html')
